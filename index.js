f9b04b5ece156f6131bb55f7b2c4f6f6f5738bba
Paul Bosorogan <bosorogan.paul@gmail.com>
2022-10-25 20:14:59 -0400

    commit tree 390f252c086c9125b30538c058c1616ba8d67527

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,6 +16,8 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  the sum of 2 elements inside the array equals target
+  elem 1 + elem 2 === target ? true : false
 */
 
 /*
